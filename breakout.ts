8f2a13bbea237d5eea0700de382e389978e1aa7b
yitznewton <yitznewton@hotmail.com>
2017-06-30 15:07:21 -0400

    Bounce

--- breakout.ts
+++ breakout.ts
@@ -1,3 +1,7 @@
+function clear(context: CanvasRenderingContext2D, canvas: HTMLCanvasElement) {
+  context.clearRect(0, 0, canvas.width, canvas.height);
+}
+
 function drawRectangle(context: CanvasRenderingContext2D, x: number, y: number, w: number, h: number, color: string) {
   context.beginPath();
   context.rect(x, y, w, h);
@@ -6,16 +10,14 @@ function drawRectangle(context: CanvasRenderingContext2D, x: number, y: number, 
   context.closePath();
 }
 
-function drawArc(
-  context: CanvasRenderingContext2D,
-  x: number,
-  y: number,
-  r: number,
-  startAngle: number,
-  endAngle: number,
-  counterClockwise: boolean,
-  color: string
-) {
+function drawArc(context: CanvasRenderingContext2D,
+                 x: number,
+                 y: number,
+                 r: number,
+                 startAngle: number,
+                 endAngle: number,
+                 counterClockwise: boolean,
+                 color: string) {
   context.beginPath();
   context.arc(x, y, r, startAngle, endAngle, counterClockwise);
   context.fillStyle = color;
@@ -30,5 +32,47 @@ function drawCircle(context: CanvasRenderingContext2D, x: number, y: number, r: 
 const canvas: HTMLCanvasElement = document.getElementById('gameCanvas') as HTMLCanvasElement;
 const context: CanvasRenderingContext2D = canvas.getContext('2d');
 
-drawRectangle(context, 20, 40, 50, 50, '#ff0000');
-drawCircle(context, 240, 160, 20, 'green');
+var x = canvas.width / 2;
+var y = canvas.height - 30;
+var color = '#0095DD';
+const r = 10;
+const dx = 2;
+const dy = -2;
+
+function randomColor(): string {
+  return '#' + Math.floor(Math.random() * 16777215).toString(16);
+}
+const draw = () => {
+  function anticipateWallCollisions() {
+    if (y + dy < 0) {
+      dy = -dy;
+      color = randomColor();
+    }
+    if (y + dy > canvas.height) {
+      dy = -dy;
+      color = randomColor();
+    }
+    if (x + dx < 0) {
+      dx = -dx;
+      color = randomColor();
+    }
+    if (x + dx > canvas.width) {
+      dx = -dx;
+      color = randomColor();
+    }
+  }
+
+  function setNextCenter() {
+    x += dx;
+    y += dy;
+  }
+
+  clear(context, canvas);
+  drawCircle(context, x, y, r, color);
+  anticipateWallCollisions();
+  setNextCenter();
+
+  requestAnimationFrame(draw);
+};
+
+requestAnimationFrame(draw);
